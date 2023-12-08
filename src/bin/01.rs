3c95bc2b1f1f2a7a7cbdac5aa9330ec237745a94
Paul David <paul.david@magenta.de>
2023-12-08 21:13:55 +0100

    Updated day 1 implementation

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,27 +4,13 @@ extern crate regex;
 
 advent_of_code::solution!(1);
 
-fn replace_number_strings(input: &str) -> String {
-    input
-        .to_lowercase()
-        .replace("one", "1")
-        .replace("two", "2")
-        .replace("three", "3")
-        .replace("four", "4")
-        .replace("five", "5")
-        .replace("six", "6")
-        .replace("seven", "7")
-        .replace("eight", "8")
-        .replace("nine", "9")
-}
-
 pub fn part_one(input: &str) -> Option<u32> {
     let sum = input
         // first, we split our string into lines
         .lines()
         .filter_map(|line| {
             // we take each line and filter all numerical values from it
-            let numbers: Vec<_> = line.chars().filter(|c| c.is_numeric()).collect();
+            let numbers: Vec<_> = line.chars().filter(|c| c.is_ascii_digit()).collect();
 
             // we then take the first value
             let left_opt = numbers.first();
@@ -57,10 +43,12 @@ pub fn part_two(input: &str) -> Option<u32> {
         .to_lowercase()
         // first, we split our string into lines
         .lines()
-        .filter_map(|line| {
+        .map(|line| {
             // we take each line and filter all numerical values from it
             let numbers: Vec<u32> = re
+                // find all regex matches in thre string
                 .find_iter(line)
+                // map every match by replacing it with the numerical value
                 .map(|m| match m.as_str() {
                     "one" | "1" => 1,
                     "two" | "2" => 2,
@@ -76,19 +64,17 @@ pub fn part_two(input: &str) -> Option<u32> {
                 .collect();
 
             // we then take the first value
-            let left_opt = numbers.first();
-
             // if a digit is found, use it as the left part
-            if let Some(left) = left_opt {
+            if let Some(left) = numbers.first() {
                 // try to find the last digit, but fall back to the
                 // already found left value if none is found
                 let right = numbers.last().unwrap_or(left);
 
-                // construct the number string from the left and right part
-                Some(left * 10 + right)
+                // construct the number from the left and right part
+                left * 10 + right
             } else {
                 // if no digit was found in the string, filter out the line
-                None
+                panic!("no digits found in {}", line);
             }
         })
         // sum it all up
@@ -115,9 +101,11 @@ mod tests {
         assert_eq!(result, Some(54500));
     }
 
+    // taken straight from the example
     #[test_case("xtwone3four" => Some(24))]
     #[test_case("zoneight234" => Some(14))]
     #[test_case("7pqrstsixteen" => Some(76))]
+    #[test_case("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen" => Some(281))]
     fn test_part_two_single(input: &str) -> Option<u32> {
         part_two(input)
     }
